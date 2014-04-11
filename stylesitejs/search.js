da86b6353b0a4dc959f76fdfb80c5bedf6d09c65
Melissa Pallay <melissapallay@gmail.com>
2014-04-11 17:32:11 -0400

    second commit

---
+++ stylesitejs/search.js
@@ -0,0 +1,115 @@
+//LIST START
+
+var full_list = [
+    ["chignon twist","night","medium","wavy","up_do","http://www.fashiondivadesign.com/wp-content/uploads/2013/08/prom-hair-tutorial-chignon-updo-600x736.jpg"],
+    ["punky pony","day","medium","wavy","up_do","http://fancy.mk/Files/Hair/Punky-Ponytail-Hair-Tutorial1.jpg"],
+    ["side braid","day","medium","wavy","up_do","http://www.hairromance.com/wp-content/uploads/2012/08/Hair-Romance-side-braided-hairstyle-tutorial.jpg"],
+    ["waterfall braid in braid","day","medium","straight","half","http://www.topdreamer.com/wp-content/uploads/2013/10/999172_387615094694482_1283612750_n1-634x634.jpg"],
+    ["fast fishtail braid","day","medium","wavy","up_do","http://www.makeupwearables.com/wp-content/uploads/2013/11/how-to-fishtail-braid-your-own-hair-tutorial-video-hairstyles.jpg"],
+    ["fancy ponytail","day","long","straight","up_do","http://www.fashionmagazine.com/wp-content/uploads/2013/04/prom-hair-tutorial-fancy-ponytail-600x600.jpg"],
+    ["messy fishtail updo","night","long","straight","up_do","http://1.bp.blogspot.com/-7EWFhYQzzXI/UXv3KeISR1I/AAAAAAAAGbY/Swdyk7W7J-Q/s1600/messy+fishtail+updo.PNG"],
+    ["sixties beehive with low ponytail","end","long","straight","up_do","http://www.bobbyglam.com/blog/wp-content/uploads/2013/05/Sixties-Beehive-with-low-ponytail.jpg"],
+    ["triple braid","day","long","wavy","up_do","http://www.hairromance.com/wp-content/uploads/2012/12/Hair-Romance-triple-braid-tutorial.jpg"],
+    ["chalked crown hair","end","medium","wavy","half","http://media-cache-ec0.pinimg.com/736x/3e/0c/0e/3e0c0e3b4c3c96b03145ba27b364dc4e.jpg"],
+    ["topsy-turvy ponytail","day","medium","wavy","up_do","http://www.hairromance.com/wp-content/uploads/2013/02/Hairstyle-Tutorial-Topsy-Turvy-Ponytail-How-to.jpg"],
+    ["vintage head wrap","day","medium","wavy","up_do","http://theglamoroushousewife.com/wp-content/uploads/2013/06/vintage-hairstyle-9.jpg"],
+    ["mini bow","day","long","wavy","half","http://cloud4.collegefashion.net/wp-content/uploads/2013/05/hair-bow-tutorial.jpg"],
+    ["braid to bun","end","long","wavy","up_do","http://24.media.tumblr.com/tumblr_m6iabxEwfN1rptw4do1_500.jpg"],
+    ["butterfly chignon","end","long","wavy","up_do","http://www.makeupwearables.com/wp-content/uploads/2013/11/Butterfly-braided-bun-tutorials-for-medium-long-hair-cute-hairstyles.jpg"],
+    ["4 twisted buns","day","long","wavy","up_do","http://www.stylemotivation.com/wp-content/uploads/2013/06/236.jpg"],
+    ["waterfall plait","night","medium","wavy","half","http://www.hairromance.com/wp-content/uploads/2013/04/Hair-Romance-Waterfall-Plait-hairstyle-tutorial.jpg"],
+    ["twisted ponytail in curly hair","day","medium","curly","up_do","http://www.hairromance.com/wp-content/uploads/2013/05/Hair-Romance-Twisted-Ponytail-Hair-Tutorial-in-curly-hair.jpg"],
+    ["bow braids","night","medium","straight","half","http://www.hairromance.com/wp-content/uploads/2013/03/Hair-Romance-Bow-braids-hairstyle-tutorial-how-to.jpg"],
+    ["braid headband","day","medium","wavy","half","http://1.bp.blogspot.com/-90MsyONYf2c/UKTwrUv4WSI/AAAAAAAAARk/-ywB7t_isUo/s640/Braid+Headband+Hair+Tutorial.JPG"],
+    ["runway bun","night","long","wavy","up_do","http://www.howtohairgirl.com/wp-content/uploads/2012/11/run-bun-300x300.jpg"],
+    ["headband updo","night","long","wavy","up_do","http://www.fantasticviewpoint.com/wp-content/uploads/2013/09/tumblr_mohyy6NcY61rasq9bo1_500.jpg"],
+    ["braid to top bun","end","medium","wavy","up_do","http://data3.whicdn.com/images/28848870/large.jpg"],
+    ["messy pigtail braids","day","short","wavy","up_do","http://manouvellemode.com/wp-content/uploads/2013/05/PicMonkey-Collage.jpg"],
+    ["faux fring/bangs","day","short","straight","up_do","http://content.latest-hairstyles.com/wp-content/uploads/2013/06/FauxFringe_Hair-Romance-Prada-inspired-faux-fringe-tutorial.jpg"],
+    ["fake bangs","day","medium","straight","up_do","http://dy6g3i6a1660s.cloudfront.net/IcEvlwO44xFYAHJP99Tz6wfQB9A/cl-4b/how-to-fake-having-bangs-with-a-hair-bun-tutorial-demi-lovato-inspired.jpg"],
+    ["short story braid","night","short","wavy","up_do","http://31.media.tumblr.com/3257347a81b2b791d59277555bcfe96e/tumblr_mnv8osFWXF1qdwkufo1_1280.jpg"],
+    ["wedding curls","end","long","curly","down","http://yooand.com/wp-content/uploads/2014/01/make--wedding-hairstyles-for-long-hair-tutorial.jpg"],
+    ["faux short hair with bow","night","medium","wavy","half","http://womandot.com/wp-content/uploads/2013/11/12.jpg"],
+    ["braid bun for my shorties","night","short","wavy","up_do","http://www.diyrefashion.com/assets/user_images/522277c2a0b0e.jpg"],
+    ["easy poofy bun","end","short","straight","up_do","http://content.latest-hairstyles.com/wp-content/uploads/2013/08/EasyPoofyBun.jpg"],
+    ["no heat curls","end","medium","wavy","down","http://prettygossip.com/wp-content/uploads/2013/03/No-Heat-Curls.jpg"],
+    ["sock bun","night","medium","wavy","up_do","http://madeinpretoria.files.wordpress.com/2011/10/thesockbun_madeinpretoria.jpg"],
+    ["braid to sock bun","end","medium","wavy","up_do","http://static1.beautifulshoes.org/wp-content/uploads/2014/02/braid-into-a-sock-bun-592x412.jpg"],
+    ["sock bun with bow","day","medium","wavy","up_do","http://pinkmartinicollection.com/blog/wp-content/uploads/2013/02/sock-bun-with-bow.jpg"],
+    ["hair bow","end","long","wavy","up_do","http://www.shopchiyo.com/wp-content/uploads/2013/04/hair-bow.jpg"],
+    ["half up hair bow","night","medium","wavy","half","http://www.fashionisers.com/wp-content/uploads/2012/10/how_to_style_a_half_up_hair_bow.jpg"],
+    ["half up hair bow ponytail","day","medium","wavy","half","http://media-cache-ec0.pinimg.com/236x/b7/e8/e7/b7e8e7c99ce7514a4759717453380020.jpg"],
+    ["vintage half up","night","long","curly","half","http://www.bobbyglam.com/blog/wp-content/uploads/2013/01/Vintage-Half-Up.jpg"],
+    ["beach babe waves","day","medium","curly","down","http://content.latest-hairstyles.com/wp-content/uploads/2013/05/MessyChunky2.jpg"],
+    ["the khaleesi braid","day","long","wavy","up_do","http://www.jadabeauty.com/blog/wp-content/uploads/2013/11/khaleesi-braid-collage-final.png"],
+    ["four strand braid","night","long","wavy","half","http://www.bobbyglam.com/blog/wp-content/uploads/2012/12/four-strand-braid-1.jpg"],
+    ["four strand braid with ribbon","end","long","wavy","half","http://3.bp.blogspot.com/-7lwFgIGx4x4/UCssZbVcu1I/AAAAAAAAE_I/R8vqjMnLwkA/s1600/four+strand+braid+tutorial+1.jpg"],
+    ["roller blow out","night","long","wavy","down","http://content.latest-hairstyles.com/wp-content/uploads/2014/03/RollerBlowOut.jpg"],
+    ["basic blowout","day","medium","wavy","down","http://www.prettydesigns.com/wp-content/uploads/2014/01/Basic-Blowout.jpg"],
+    ["knotted ponytail","night","long","straight","half","http://sassyhongkong.com/wp-content/uploads/2012/05/knotted-ponytail-tutorial.png"],
+    ["longer fuller ponytail","end","long","wavy","up_do","http://www.makeup.com/wp-content/uploads/2013/10/the_longer_fuller_ponytail_tutorial.jpg"],
+    ["low ponytail","day","medium","wavy","half","http://www.fashiondivadesign.com/wp-content/uploads/2014/01/LowWovenPonytail.jpg"],
+    ["knot scarf head wrap","end","medium","wavy","down","http://www.topdreamer.com/wp-content/uploads/2014/03/hairstyle-with-bandanapart3.2.jpg"]  
+]
+
+var length, texture, time, type
+//LIST END
+
+function make_list() { //find selected details   
+
+    for (var i = 1; i <= 3; i++) {
+        if (document.getElementById("radio"+i).checked) {
+            time = document.getElementById("radio"+i).value;
+        }
+    };        
+    for (var i = 4; i <= 6; i++) {
+        if (document.getElementById("radio"+i).checked) {
+            length = document.getElementById("radio"+i).value;
+        }
+    };  
+    for (var i = 7; i <= 9; i++) {
+        if (document.getElementById("radio"+i).checked) {
+            texture = document.getElementById("radio"+i).value;
+        }
+    };  
+    for (var i = 10; i <= 12; i++) {
+        if (document.getElementById("radio"+i).checked) {
+            type = document.getElementById("radio"+i).value;
+        }
+    };  
+    
+    return [time, length, texture, type];
+}
+
+function search() {
+    var image_list = [];   
+    var details = make_list();
+    for (var i=0; i < full_list.length; i++) {
+        if (details[0] == full_list[i][1] && details[1] == full_list[i][2] && details[2] == full_list[i][3] && details[3] == full_list[i][4]) {
+            image_list.push(full_list[i][5])
+        }
+    };
+    if (image_list.length >= 1) {
+        image = image_list.pop()
+        var page_display = ("<button onclick='next("+ [(JSON.stringify(image_list))] +")'><img src='" + image + "'></button>");
+        document.getElementById("t2").innerHTML=page_display;
+    }
+    else {
+        sorry()
+    }
+}
+
+function sorry() {    
+    document.getElementById("t2").innerHTML="<h2>Oh no, we don't have anything that matches that yet, sorry...</h2><br><a href='search.html'><img src='http://www.calbuzz.com/wp-content/uploads/sorry.jpg'></a>";
+}
+
+function next(image_list) {
+    if (image_list.length >= 1) {
+        image = image_list.pop()
+        var page_display = ("<button onclick='next("+ [(JSON.stringify(image_list))] +")'><img src='" + image + "'></button>");
+        document.getElementById("t2").innerHTML=page_display;
+    }
+    else {
+        sorry()
+    }
+}
